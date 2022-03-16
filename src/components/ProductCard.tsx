cfb68c0b7d29e58e777dafc4af0c0ef3d6d69e2f
Stivenprod <asdm1098@gmail.com>
2022-03-15 23:03:49 -0500

    Version 1.0.0-rc lista

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -19,8 +19,7 @@ export interface Props {
 }
 
 export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
-    const { counter, increaseBy, maxCount, isMaxCountReached, reset } 
-    = useProducts({ onChange, product, value, initialValues });
+    const { counter, increaseBy, maxCount, isMaxCountReached, reset } = useProducts({ onChange, product, value, initialValues });
     return (
         <Provider value={{ 
             counter, increaseBy, product, maxCount
